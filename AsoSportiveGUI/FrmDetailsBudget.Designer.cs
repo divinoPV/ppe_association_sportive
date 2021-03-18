72c6cb979fc8f91e71fa890a8102c38be6d2f500
divinoPV <monteiro.hugo2001@icloud.com>
2021-03-18 08:55:38 +0100

    stash(change)

---
+++ AsoSportiveGUI/FrmDetailsBudget.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace AsoSportiveGUI
+{
+    partial class FrmDetailsBudget
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmDetailsBudget));
+            this.btnRetour = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblFlux = new System.Windows.Forms.Label();
+            this.btnAddFlux = new System.Windows.Forms.Button();
+            this.pnl = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnRetour
+            // 
+            this.btnRetour.Location = new System.Drawing.Point(688, 32);
+            this.btnRetour.Name = "btnRetour";
+            this.btnRetour.Size = new System.Drawing.Size(75, 23);
+            this.btnRetour.TabIndex = 6;
+            this.btnRetour.Text = "Retour";
+            this.btnRetour.UseVisualStyleBackColor = false;
+            this.btnRetour.Click += new System.EventHandler(this.btnRetour_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAdd.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAdd.Location = new System.Drawing.Point(576, 32);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 7;
+            this.btnAdd.Text = "Ajouter";
+            this.btnAdd.UseVisualStyleBackColor = false;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 34F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(16, 16);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(171, 53);
+            this.lblTitle.TabIndex = 8;
+            this.lblTitle.Text = "Budget";
+            // 
+            // lblFlux
+            // 
+            this.lblFlux.AutoSize = true;
+            this.lblFlux.Font = new System.Drawing.Font("Microsoft Sans Serif", 21F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFlux.Location = new System.Drawing.Point(0, 0);
+            this.lblFlux.Name = "lblFlux";
+            this.lblFlux.Size = new System.Drawing.Size(69, 32);
+            this.lblFlux.TabIndex = 9;
+            this.lblFlux.Text = "Flux";
+            // 
+            // btnAddFlux
+            // 
+            this.btnAddFlux.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btnAddFlux.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnAddFlux.Location = new System.Drawing.Point(96, 8);
+            this.btnAddFlux.Name = "btnAddFlux";
+            this.btnAddFlux.Size = new System.Drawing.Size(88, 23);
+            this.btnAddFlux.TabIndex = 10;
+            this.btnAddFlux.Text = "Ajouter un flux";
+            this.btnAddFlux.UseVisualStyleBackColor = false;
+            this.btnAddFlux.Click += new System.EventHandler(this.btnAddFlux_Click);
+            // 
+            // pnl
+            // 
+            this.pnl.AutoScroll = true;
+            this.pnl.AutoSize = true;
+            this.pnl.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.pnl.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.pnl.ColumnCount = 4;
+            this.pnl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.pnl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.pnl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.pnl.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.pnl.Location = new System.Drawing.Point(3, 3);
+            this.pnl.Name = "pnl";
+            this.pnl.RowCount = 1;
+            this.pnl.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.pnl.Size = new System.Drawing.Size(10, 4);
+            this.pnl.TabIndex = 2;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoSize = true;
+            this.flowLayoutPanel1.Controls.Add(this.pnl);
+            this.flowLayoutPanel1.Controls.Add(this.panel1);
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(24, 80);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(190, 48);
+            this.flowLayoutPanel1.TabIndex = 11;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lblFlux);
+            this.panel1.Controls.Add(this.btnAddFlux);
+            this.panel1.Location = new System.Drawing.Point(3, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(184, 32);
+            this.panel1.TabIndex = 12;
+            // 
+            // FrmDetailsBudget
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(780, 557);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnRetour);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "FrmDetailsBudget";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Détails Budget";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnRetour;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblFlux;
+        private System.Windows.Forms.Button btnAddFlux;
+        private System.Windows.Forms.TableLayoutPanel pnl;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ AsoSportiveGUI/FrmDetailsBudget.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using AsoSportiveBLL;
+using AsoSportiveBO;
+using System.Configuration;
+
+namespace AsoSportiveGUI
+{
+    public partial class FrmDetailsBudget : Form
+    {
+        public FrmDetailsBudget()
+        {
+            InitializeComponent();
+
+            GestionUtilisateurs.SetchaineConnexion(ConfigurationManager.ConnectionStrings["Utilisateur"]);
+            List<Budget> budgets = GestionBudget.GetLesBudget();
+
+            int x = 20;
+            int y = 20;
+
+            foreach (Budget budget in budgets)
+            {
+                y += 20;
+
+                Label lblLibelle = new Label();
+                lblLibelle.Text = budget.Libelle.Trim();
+                lblLibelle.AutoSize = true;
+                lblLibelle.Location = new Point(x, y);
+                lblLibelle.Dock = DockStyle.Fill;
+                lblLibelle.TextAlign = ContentAlignment.MiddleCenter;
+
+                Label lblMontantInitial = new Label();
+                lblMontantInitial.Text = budget.MontantInitial.ToString();
+                lblMontantInitial.AutoSize = true;
+                lblMontantInitial.Location = new Point(x, y);
+                lblMontantInitial.Dock = DockStyle.Fill;
+                lblMontantInitial.TextAlign = ContentAlignment.MiddleCenter;
+
+                Button btnUpdate = new Button();
+                btnUpdate.Tag = budget;
+                btnUpdate.Text = "Modifier";
+                btnUpdate.AutoSize = true;
+                btnUpdate.Location = new Point(x, y);
+                btnUpdate.Dock = DockStyle.Fill;
+                btnUpdate.Click += new EventHandler(btnUpdate_Click);
+                btnUpdate.TextAlign = ContentAlignment.MiddleCenter;
+
+                Button btnDelete = new Button();
+                btnDelete.Tag = budget.Id;
+                btnDelete.Text = "Supprimer";
+                btnDelete.AutoSize = true;
+                btnDelete.Location = new Point(x, y);
+                btnDelete.Dock = DockStyle.Fill;
+                btnDelete.Click += new EventHandler(btnDelete_Click);
+                btnDelete.TextAlign = ContentAlignment.MiddleCenter;
+
+                this.Controls.Find("pnl", true)[0].Controls.Add(lblLibelle);
+                this.Controls.Find("pnl", true)[0].Controls.Add(lblMontantInitial);
+                this.Controls.Find("pnl", true)[0].Controls.Add(btnUpdate);
+                this.Controls.Find("pnl", true)[0].Controls.Add(btnDelete);
+            }
+        }
+
+        public void btnUpdate_Click(object sender, EventArgs e)
+        {
+            Button senderButton = sender as Button;
+        }
+
+        public void btnDelete_Click(object sender, EventArgs e)
+        {
+            Button senderButton = sender as Button;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // fermeture du formulaire actuel
+            FrmAjoutEleve frmAjoutEleve = new FrmAjoutEleve();
+            frmAjoutEleve.Show(); // ouverture du formulaire
+        }
+
+        private void btnRetour_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // fermeture du formulaire actuel
+            FrmRedirection frmRedirection = new FrmRedirection();
+            frmRedirection.Show(); // ouverture du formulaire
+        }
+
+        private void btnAddFlux_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // fermeture du formulaire actuel
+            FrmAjoutFlux frmAjoutFlux = new FrmAjoutFlux();
+            frmAjoutFlux.Show(); // ouverture du formulaire  
+        }
+    }
+}
