771aea0f4057746de60e9a388d92f896af9f21ad
divinoPV <monteiro.hugo2001@icloud.com>
2021-04-20 18:00:11 +0200

    build(class)[technique]{utilitaire}

--- AsoSportiveGUI/FrmGestionCompta.cs
+++ AsoSportiveGUI/FrmGestionCompta.cs
@@ -15,7 +15,6 @@ namespace AsoSportiveGUI
 {
     public partial class FrmGestionCompta : Form
     {
-        public const int Coord = 20;
         public const string PnlResultat = "pnlResultat";
 
         public FrmGestionCompta()
@@ -36,79 +35,57 @@ namespace AsoSportiveGUI
                     pnlResultat.Controls.RemoveAt(pnlResultat.Controls.Count - 1);
                 }
 
-                this.resultat();
+                this.setPnlAdherent();
             }
-        }
-
-        private void resultat()
-        {
-            this.setPnlAdherent();
         }
 
         private void setPnlResultatLabel()
         {
             List<Label> labels = new List<Label>();
-            labels.Add(this.setLabel("Nom complet"));
-            labels.Add(this.setLabel("Nom"));
-            labels.Add(this.setLabel("Prénom"));
-            labels.Add(this.setLabel("Née le"));
-            labels.Add(this.setLabel("N° tel"));
-            labels.Add(this.setLabel("Email"));
-            labels.Add(this.setLabel("N° tel parent"));
-            labels.Add(this.setLabel("Prélèvement automatique"));
-            labels.Add(this.setLabel("Genre"));
-            labels.Add(this.setLabel("Modifier le"));
-            labels.Add(this.setLabel("Archiver"));
-            labels.Add(this.setLabel("Créer par"));
-            labels.Add(this.setLabel("Classe"));
+            labels.Add(FrmUtilitaire.setLabel("Nom complet"));
+            labels.Add(FrmUtilitaire.setLabel("Nom"));
+            labels.Add(FrmUtilitaire.setLabel("Prénom"));
+            labels.Add(FrmUtilitaire.setLabel("Née le"));
+            labels.Add(FrmUtilitaire.setLabel("N° tel"));
+            labels.Add(FrmUtilitaire.setLabel("Email"));
+            labels.Add(FrmUtilitaire.setLabel("N° tel parent"));
+            labels.Add(FrmUtilitaire.setLabel("Prélèvement automatique"));
+            labels.Add(FrmUtilitaire.setLabel("Genre"));
+            labels.Add(FrmUtilitaire.setLabel("Modifier le"));
+            labels.Add(FrmUtilitaire.setLabel("Archiver"));
+            labels.Add(FrmUtilitaire.setLabel("Créer par"));
+            labels.Add(FrmUtilitaire.setLabel("Classe"));
 
-            this.putLabelInPnl(labels);
+            FrmUtilitaire.init().putLabelInPnl(labels, this.findControl(PnlResultat));
         }
 
         private void setPnlAdherent()
         {
             List<Adherent> adherents = GestionAdherent.GetAdherentRecherche(this.txtRecherche.Text);
 
-            int y = Coord;
+            int y = FrmUtilitaire.Coord;
 
             foreach (Adherent adherent in adherents)
             {
                 List<Label> labels = new List<Label>();
-                labels.Add(this.setLabel(adherent.Nom.Trim() + ' ' + adherent.Prenom.Trim(), y: Coord));
-                labels.Add(this.setLabel(adherent.Nom.Trim(), y: y));
-                labels.Add(this.setLabel(adherent.Prenom.Trim(), y: y));
-                labels.Add(this.setLabel(adherent.Ddn.ToString(), y: y));
-                labels.Add(this.setLabel(adherent.NumTel.Trim(), y: y));
-                labels.Add(this.setLabel(adherent.Email.Trim(), y: y));
-                labels.Add(this.setLabel(adherent.NumParnt.Trim(), y: y));
-                labels.Add(this.setLabel(adherent.AutPrelev.ToString(), y: y));
-                labels.Add(this.setLabel(adherent.Sexe.ToString(), y: y));
-                labels.Add(this.setLabel(adherent.DateMaj.ToString(), y: y));
-                labels.Add(this.setLabel(adherent.Archive.ToString(), y: y));
-                labels.Add(this.setLabel(adherent.Utilisateur.Login.ToString(), y: y));
-                labels.Add(this.setLabel(adherent.Classe.Libelle.Trim(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.Nom.Trim() + ' ' + adherent.Prenom.Trim(), y: FrmUtilitaire.Coord));
+                labels.Add(FrmUtilitaire.setLabel(adherent.Nom.Trim(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.Prenom.Trim(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.Ddn.ToString(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.NumTel.Trim(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.Email.Trim(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.NumParnt.Trim(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.AutPrelev.ToString(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.Sexe.ToString(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.DateMaj.ToString(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.Archive.ToString(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.Utilisateur.Login.ToString(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.Classe.Libelle.Trim(), y: y));
 
                 y += 20;
 
-                this.putLabelInPnl(labels);
+                FrmUtilitaire.init().putLabelInPnl(labels, this.findControl(PnlResultat));
             }
-        }
-
-        private void putLabelInPnl(List<Label> labels)
-        {
-            foreach (Label label in labels) { this.Controls.Find(PnlResultat, true)[0].Controls.Add(label); }
-        }
-
-        private Label setLabel(string text, int x = Coord, int y = Coord)
-        {
-            Label lbl = new Label();
-            lbl.Text = text;
-            lbl.AutoSize = true;
-            lbl.Location = new Point(x, y);
-            lbl.Dock = DockStyle.Fill;
-            lbl.TextAlign = ContentAlignment.MiddleCenter;
-
-            return lbl;
         }
 
         private void btnRetour_Click(object sender, EventArgs e)
@@ -116,6 +93,11 @@ namespace AsoSportiveGUI
             this.Hide(); // fermeture du formulaire actuel
             FrmRedirection frmRedirection = new FrmRedirection();
             frmRedirection.Show(); // ouverture du formulaire
+        }
+
+        public Control[] findControl(string key, bool boolean = true)
+        {
+            return this.Controls.Find(key, boolean);
         }
     }
 }
---
+++ AsoSportiveGUI/FrmUtilitaire.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsoSportiveGUI
+{
+    public partial class FrmUtilitaire : Form
+    {
+        public const int Coord = 20;
+
+        public static FrmUtilitaire init()
+        {
+            return new FrmUtilitaire();
+        }
+
+        public static Label setLabel(string text, int x = Coord, int y = Coord)
+        {
+            Label lbl = new Label();
+            lbl.Text = text;
+            lbl.AutoSize = true;
+            lbl.Location = new Point(x, y);
+            lbl.Dock = DockStyle.Fill;
+            lbl.TextAlign = ContentAlignment.MiddleCenter;
+
+            return lbl;
+        }
+
+        public void putLabelInPnl(List<Label> labels, Control[] control)
+        {
+            foreach (Label label in labels) { control[0].Controls.Add(label); }
+        }
+    }
+}
