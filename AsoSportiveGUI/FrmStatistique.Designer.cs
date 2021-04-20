75315eded9053acf602219cb961b2bb0daa5d35c
divinoPV <monteiro.hugo2001@icloud.com>
2021-04-20 19:20:35 +0200

    build(statistique)[nb event, repartition]

--- AsoSportiveGUI/FrmStatistique.Designer.cs
+++ AsoSportiveGUI/FrmStatistique.Designer.cs
@@ -29,19 +29,101 @@ namespace AsoSportiveGUI
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnRetour = new System.Windows.Forms.Button();
+            this.lblStatistique = new System.Windows.Forms.Label();
+            this.pnlStatistiqueParEleve = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.pnlStatistiqueGenre = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // btnRetour
+            // 
+            this.btnRetour.Location = new System.Drawing.Point(713, 12);
+            this.btnRetour.Name = "btnRetour";
+            this.btnRetour.Size = new System.Drawing.Size(75, 23);
+            this.btnRetour.TabIndex = 0;
+            this.btnRetour.Text = "Retour";
+            this.btnRetour.UseVisualStyleBackColor = true;
+            this.btnRetour.Click += new System.EventHandler(this.btnRetour_Click);
+            // 
+            // lblStatistique
+            // 
+            this.lblStatistique.AutoSize = true;
+            this.lblStatistique.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStatistique.Location = new System.Drawing.Point(12, 22);
+            this.lblStatistique.Name = "lblStatistique";
+            this.lblStatistique.Size = new System.Drawing.Size(208, 25);
+            this.lblStatistique.TabIndex = 0;
+            this.lblStatistique.Text = "Statistique par élève";
+            // 
+            // pnlStatistiqueParEleve
+            // 
+            this.pnlStatistiqueParEleve.AutoScroll = true;
+            this.pnlStatistiqueParEleve.AutoSize = true;
+            this.pnlStatistiqueParEleve.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.pnlStatistiqueParEleve.ColumnCount = 3;
+            this.pnlStatistiqueParEleve.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.pnlStatistiqueParEleve.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.pnlStatistiqueParEleve.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.pnlStatistiqueParEleve.Location = new System.Drawing.Point(3, 3);
+            this.pnlStatistiqueParEleve.Name = "pnlStatistiqueParEleve";
+            this.pnlStatistiqueParEleve.RowCount = 1;
+            this.pnlStatistiqueParEleve.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.pnlStatistiqueParEleve.Size = new System.Drawing.Size(8, 4);
+            this.pnlStatistiqueParEleve.TabIndex = 1;
+            // 
+            // flowLayoutPanel
+            // 
+            this.flowLayoutPanel.AutoSize = true;
+            this.flowLayoutPanel.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.flowLayoutPanel.Controls.Add(this.pnlStatistiqueParEleve);
+            this.flowLayoutPanel.Controls.Add(this.pnlStatistiqueGenre);
+            this.flowLayoutPanel.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel.Location = new System.Drawing.Point(17, 50);
+            this.flowLayoutPanel.Name = "flowLayoutPanel";
+            this.flowLayoutPanel.Size = new System.Drawing.Size(14, 20);
+            this.flowLayoutPanel.TabIndex = 2;
+            // 
+            // pnlStatistiqueGenre
+            // 
+            this.pnlStatistiqueGenre.AutoScroll = true;
+            this.pnlStatistiqueGenre.AutoSize = true;
+            this.pnlStatistiqueGenre.CellBorderStyle = System.Windows.Forms.TableLayoutPanelCellBorderStyle.Inset;
+            this.pnlStatistiqueGenre.ColumnCount = 2;
+            this.pnlStatistiqueGenre.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.pnlStatistiqueGenre.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle());
+            this.pnlStatistiqueGenre.Location = new System.Drawing.Point(3, 13);
+            this.pnlStatistiqueGenre.Name = "pnlStatistiqueGenre";
+            this.pnlStatistiqueGenre.RowCount = 1;
+            this.pnlStatistiqueGenre.RowStyles.Add(new System.Windows.Forms.RowStyle());
+            this.pnlStatistiqueGenre.Size = new System.Drawing.Size(8, 4);
+            this.pnlStatistiqueGenre.TabIndex = 2;
             // 
             // FrmStatistique
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel);
+            this.Controls.Add(this.lblStatistique);
+            this.Controls.Add(this.btnRetour);
             this.Name = "FrmStatistique";
             this.Text = "Statistique";
+            this.flowLayoutPanel.ResumeLayout(false);
+            this.flowLayoutPanel.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnRetour;
+        private System.Windows.Forms.Label lblStatistique;
+        private System.Windows.Forms.TableLayoutPanel pnlStatistiqueParEleve;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel;
+        private System.Windows.Forms.TableLayoutPanel pnlStatistiqueGenre;
     }
 }
--- AsoSportiveGUI/FrmStatistique.cs
+++ AsoSportiveGUI/FrmStatistique.cs
@@ -1,20 +1,113 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AsoSportiveBLL;
+using AsoSportiveBO;
 
 namespace AsoSportiveGUI
 {
     public partial class FrmStatistique : Form
     {
+        public const string PnlStatistiqueParEleve = "pnlStatistiqueParEleve";
+        public const string PnlStatistiqueGenre = "pnlStatistiqueGenre";
+
         public FrmStatistique()
         {
             InitializeComponent();
+
+            GestionUtilisateurs.SetchaineConnexion(ConfigurationManager.ConnectionStrings["Utilisateur"]);
+
+            this.setPnlStatistiqueParEleveLabel();
+            this.setPnlStatistiqueParEleveResultat();
+
+            this.setPnlStatistiqueGenreLabel();
+            this.setPnlStatistiqueGenreResultat();
+        }
+
+        private void setPnlStatistiqueParEleveLabel()
+        {
+            List<Label> labels = new List<Label>();
+            labels.Add(FrmUtilitaire.setLabel("Nom"));
+            labels.Add(FrmUtilitaire.setLabel("Prénom"));
+            labels.Add(FrmUtilitaire.setLabel("Nombre de participation aux évènements"));
+
+            FrmUtilitaire.init().putLabelInPnl(labels, this.findControl(PnlStatistiqueParEleve));
+        }
+
+        private void setPnlStatistiqueParEleveResultat()
+        {
+            List<Adherent> adherents = GestionAdherent.GetAdherent();
+
+            int y = FrmUtilitaire.Coord;
+
+            foreach (Adherent adherent in adherents)
+            {
+                List<Label> labels = new List<Label>();
+                labels.Add(FrmUtilitaire.setLabel(adherent.Nom.Trim(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(adherent.Prenom.Trim(), y: y));
+                labels.Add(FrmUtilitaire.setLabel(FrmUtilitaire.rand(32).ToString(), y: y));
+
+                y += 20;
+
+                FrmUtilitaire.init().putLabelInPnl(labels, this.findControl(PnlStatistiqueParEleve));
+            }
+        }
+
+        private void setPnlStatistiqueGenreLabel()
+        {
+            List<Label> labels = new List<Label>();
+            labels.Add(FrmUtilitaire.setLabel("Genre"));
+            labels.Add(FrmUtilitaire.setLabel("Nombre d'élève"));
+
+            FrmUtilitaire.init().putLabelInPnl(labels, this.findControl(PnlStatistiqueGenre));
+        }
+
+        private void setPnlStatistiqueGenreResultat()
+        {
+            Dictionary<string, int> genres = new Dictionary<string, int>();
+            genres.Add(FrmAjoutEleve.GenreHomme, 0);
+            genres.Add(FrmAjoutEleve.GenreFemme, 0);
+
+
+            List<Adherent> adherents = GestionAdherent.GetAdherent();
+
+            foreach (Adherent adherent in adherents)
+            {
+                if (adherent.Sexe == 'H') {
+                    genres[FrmAjoutEleve.GenreHomme] += 1;
+                } 
+                else {
+                    genres[FrmAjoutEleve.GenreFemme] += 1;
+                }
+            }
+
+            foreach (KeyValuePair<string, int> genre in genres)
+            {
+                List<Label> labels = new List<Label>();
+                labels.Add(FrmUtilitaire.setLabel(genre.Key));
+                labels.Add(FrmUtilitaire.setLabel(genre.Value.ToString()));
+
+                FrmUtilitaire.init().putLabelInPnl(labels, this.findControl(PnlStatistiqueGenre));
+            }
+        }
+
+        public Control[] findControl(string key, bool boolean = true)
+        {
+            return this.Controls.Find(key, boolean);
+        }
+
+        private void btnRetour_Click(object sender, EventArgs e)
+        {
+            this.Hide(); // fermeture du formulaire actuel
+            FrmRedirection frmRedirection = new FrmRedirection();
+            frmRedirection.Show(); // ouverture du formulaire
         }
     }
 }
